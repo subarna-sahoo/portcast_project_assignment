f97a02bfa40d7451991f49db152856879c7617b1
Subarna Sahoo <subarna@Subarnas-MacBook-Pro.local>
2025-10-05 17:39:59 +0530

    fix: added lifespan and urls for getter method of Settings

--- backend/commons/configs.py
+++ backend/commons/configs.py
@@ -1,10 +1,25 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from functools import lru_cache
 
+# Constants
+TOP_N_CACHED_WORDS = 100  # Number of top frequency words to keep in Redis cache
+
 
 class Settings(BaseSettings):
     # Database
     database_url: str
+
+    # Elasticsearch 
+    elasticsearch_url: str
+
+    # Redis
+    redis_url: str
+
+    # Dictionary API
+    dictionary_api_url: str #"https://api.dictionaryapi.dev/api/v2/entries/en"
+
+    # Metaphorpsum API
+    metaphorpsum_url: str #"http://metaphorpsum.com/paragraphs/1"
 
     # API
     api_host: str = "0.0.0.0"
--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
 
 from backend.ingest_service.routes import router as ingest_router
+from backend.dict_service.routes import router as dict_router
+from backend.search_service.routes import router as search_router
 
+from backend.commons.redis_client import close_redis_client, reload_word_frequencies_from_db
+from backend.commons.database import get_db
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup: Reload Redis cache from DB
+    async for db in get_db():
+        await reload_word_frequencies_from_db(db)
+        break  # Only need one iteration
+    yield
+    # Shutdown
+    await close_redis_client()
 
 app = FastAPI(
     title="Portcast Assignment API",
@@ -12,6 +28,7 @@ app = FastAPI(
     docs_url="/docs",
     redoc_url="/redoc",
     openapi_url="/openapi.json",
+    lifespan=lifespan
 )
 
 # CORS middleware
@@ -24,6 +41,8 @@ app.add_middleware(
 )
 
 app.include_router(ingest_router)
+app.include_router(search_router)
+app.include_router(dict_router)
 
 
 @app.get("/health")
